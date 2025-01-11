0f9c4ab0c978aa46023cb0ed13f2ee7296556ce0
tarcio <67603627+tarcioee@users.noreply.github.com>
2025-01-11 12:41:55 -0300

    fé e café

--- queries/queries_produto.sql
+++ queries/queries_produto.sql
@@ -3,16 +3,19 @@
 INSERT INTO Produto (tipo_produto, nm_produto, preco_produto, descricao, tamanho_pizza, volume_bebida, qtd_disponivel, porcentagem_promoção)
 VALUES ('pizza', 'Pizza Margherita', 25.50, 'Pizza com molho de tomate, mussarela e manjericão', 'Grande', NULL, 100, 10.0);
 
---Query para atualizar o estoque de um produto após venda:
+--Query para atualizar o estoque de um produto:
 UPDATE Produto
-SET qtd_disponivel = qtd_disponivel - 1
+SET qtd_disponivel = qtd_disponivel + 10
 WHERE cp_id_produto = 1;  -- substitua pelo ID do produto
 
---Query que muda o valor de qtd_disponivel de um produto
+-- Query pra retirar produtos comprados do estoque
 UPDATE Produto
-SET qtd_disponivel = qtd_disponivel + 5
-WHERE cp_id_produto = 1;  -- Substitua pelo ID do produto que deseja atualizar
+SET qtd_disponivel = qtd_disponivel - pp.qtd_compradas
+FROM Produto_pedido pp
+WHERE Produto.cp_id_produto = pp.ce_produto
+  AND pp.ce_pedido = <id_do_pedido>;
 
+--Requisito 4
 --Query para ver pizzas e aperitivos sem ingrediente do tipo "animal"
 SELECT p.nm_produto, p.tipo_produto
 FROM Produto p
